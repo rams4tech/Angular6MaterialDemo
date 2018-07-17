c5d62cd7acf59c99bbe4455f9a5cec59113388c1
Ramesh Sunkari <rasunkar@microsoft.com>
2018-07-17 21:27:59 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule } from '@angular/material';
+
+import { TableBasicExampleComponent } from './table/table-basic-example/table-basic-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableBasicExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
